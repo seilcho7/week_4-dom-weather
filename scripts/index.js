cef482364f456551e72b3699b69e1c2e404218b7
Seil Cho <seilcho7@hotmail.com>
2019-03-07 17:08:20 -0500

    adds sunrise and sunset

--- scripts/index.js
+++ scripts/index.js
@@ -42,7 +42,6 @@ const atlWeather ={
  };
 
 // Using the tdd-weather repo as your starter code, create and append DOM elements that show the following information:
-
 // city name
 // temperature
 // wind speed
@@ -68,3 +67,81 @@ weatherInfo.forEach(function (array) {
 // Write a function that accepts the entire weather object, extracts the icon information, and returns the URL for the icon.
 // Then, write a function that accepts a URL, creates DOM elements to show the image, and appends it to the page.
 
+function findUrl(obj) {
+   const url = `http://openweathermap.org/img/w/${obj.weather[0].icon}.png`;
+   return url;
+}
+
+function appendImg(findUrl) {
+   const img = document.createElement('img');
+   img.setAttribute('src', findUrl(atlWeather));
+   return aDiv.append(img);
+}
+
+appendImg(findUrl);
+
+// Bonus #2: Showing a map
+// Using the latitude and longitude functions and the information from this stack overflow answer, add a map element to the page.
+
+function mapElement(obj) {
+   const iframe = document.createElement('iframe');
+   iframe.setAttribute('width', '360');
+   iframe.setAttribute('height', '270');
+   iframe.setAttribute('frameborder', '0');
+   iframe.setAttribute('style', 'border:0');
+   iframe.setAttribute('src', `http://maps.google.com/maps?q=${obj.coord.lon}, ${obj.coord.lat}&z=15&output=embed`);
+   return aDiv.prepend(iframe);
+}
+
+mapElement(atlWeather);
+
+// Bonus #3: Converting the sunrise/sunset
+// The sunrise and sunset information is in an odd format. In order to show it in a human readable format, you'll need to do some detective work.
+
+function convert(obj){
+
+   // Unixtimestamp
+   var unixtimestamp = obj;
+  
+   // Months array
+   var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  
+   // Convert timestamp to milliseconds
+   var date = new Date(unixtimestamp*1000);
+  
+   // Year
+   var year = date.getFullYear();
+  
+   // Month
+   var month = months_arr[date.getMonth()];
+  
+   // Day
+   var day = date.getDate();
+  
+   // Hours
+   var hours = date.getHours();
+  
+   // Minutes
+   var minutes = "0" + date.getMinutes();
+  
+   // Seconds
+   var seconds = "0" + date.getSeconds();
+  
+   // Display date time in MM-dd-yyyy h:m:s format
+   var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+
+   return convdataTime;
+   
+   // document.getElementById('datetime').innerHTML = convdataTime;
+}
+
+const sunriseSunset = [
+   `Sunrise: ${convert(atlWeather.sys.sunrise)}`,
+   `Sunset: ${convert(atlWeather.sys.sunset)}`,
+]
+
+sunriseSunset.forEach(function (sun) {
+   const aPara = document.createElement('p');
+   aPara.textContent = sun;
+   aDiv.append(aPara);
+});
