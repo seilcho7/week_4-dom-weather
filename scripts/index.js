ac42d92d378ada9bfe7ddb0092b8eebeb24580a6
Seil Cho <seilcho7@hotmail.com>
2019-03-11 15:07:03 -0400

    adds weather api

--- scripts/index.js
+++ scripts/index.js
@@ -1,45 +1,45 @@
-const atlWeather ={  
-    "coord":{  
-       "lon":-84.373,
-       "lat":33.848,
-    },
-    "weather":[  
-       {  
-          "id":800,
-          "main":"Clear",
-          "description":"clear sky",
-          "icon":"01d"
-       }
-    ],
-    "base":"stations",
-    "main":{  
-       "temp":272.76,
-       "pressure":1022,
-       "humidity":40,
-       "temp_min":270.15,
-       "temp_max":275.37
-    },
-    "visibility":16093,
-    "wind":{  
-       "speed":3.1,
-       "deg":340
-    },
-    "clouds":{  
-       "all":1
-    },
-    "dt":1551797654,
-    "sys":{  
-       "type":1,
-       "id":4399,
-       "message":0.0072,
-       "country":"US",
-       "sunrise":1551787231,
-       "sunset":1551829066
-    },
-    "id":4180439,
-    "name":"Atlanta",
-    "cod":200
- };
+// const atlWeather ={  
+//     "coord":{  
+//        "lon":-84.373,
+//        "lat":33.848,
+//     },
+//     "weather":[  
+//        {  
+//           "id":800,
+//           "main":"Clear",
+//           "description":"clear sky",
+//           "icon":"01d"
+//        }
+//     ],
+//     "base":"stations",
+//     "main":{  
+//        "temp":272.76,
+//        "pressure":1022,
+//        "humidity":40,
+//        "temp_min":270.15,
+//        "temp_max":275.37
+//     },
+//     "visibility":16093,
+//     "wind":{  
+//        "speed":3.1,
+//        "deg":340
+//     },
+//     "clouds":{  
+//        "all":1
+//     },
+//     "dt":1551797654,
+//     "sys":{  
+//        "type":1,
+//        "id":4399,
+//        "message":0.0072,
+//        "country":"US",
+//        "sunrise":1551787231,
+//        "sunset":1551829066
+//     },
+//     "id":4180439,
+//     "name":"Atlanta",
+//     "cod":200
+//  };
 
 // Using the tdd-weather repo as your starter code, create and append DOM elements that show the following information:
 // city name
@@ -51,16 +51,43 @@ const aDiv = document.createElement('div');
 
 body.append(aDiv);
 
-const weatherInfo = [
-   `City Name: ${atlWeather.name}`,
-   `Temperature: ${atlWeather.main.temp}`,
-   `Wind Speed: ${atlWeather.wind.speed}`,
-]
+const URL = "https://api.openweathermap.org/data/2.5/weather?q=Atlanta, US&appid=544e633f1a1d6d7bb3378ec526f12f59";
 
-weatherInfo.forEach(function (array) {
-   const aPara = document.createElement('p');
-   aPara.textContent = array;
-   aDiv.append(aPara);
+fetch(URL)
+.then(function (response) { 
+   return response.json(); 
+})
+.then( function (weatherData) { 
+   console.log(weatherData); 
+   return weatherData;
+})
+.then(function (noReallyTheWeatherData) {
+   mapElement(noReallyTheWeatherData);
+   const weatherInfo = [
+      `City Name: ${noReallyTheWeatherData.name}`,
+      `Temperature: ${noReallyTheWeatherData.main.temp}`,
+      `Wind Speed: ${noReallyTheWeatherData.wind.speed}`,
+   ];
+   weatherInfo.forEach(function (array) {
+      const aPara = document.createElement('p');
+      aPara.textContent = array;
+      aDiv.append(aPara);
+   });
+   appendImg(findUrl);
+   function appendImg(findUrl) {
+      const img = document.createElement('img');
+      img.setAttribute('src', findUrl(noReallyTheWeatherData));
+      return aDiv.append(img);
+   };
+   const sunriseSunset = [
+      `Sunrise: ${convert(noReallyTheWeatherData.sys.sunrise)}`,
+      `Sunset: ${convert(noReallyTheWeatherData.sys.sunset)}`,
+   ]
+   sunriseSunset.forEach(function (sun) {
+      const aPara = document.createElement('p');
+      aPara.textContent = sun;
+      aDiv.append(aPara);
+   });
 });
 
 // Bonus #1: Showing an icon
@@ -71,14 +98,6 @@ function findUrl(obj) {
    const url = `http://openweathermap.org/img/w/${obj.weather[0].icon}.png`;
    return url;
 }
-
-function appendImg(findUrl) {
-   const img = document.createElement('img');
-   img.setAttribute('src', findUrl(atlWeather));
-   return aDiv.append(img);
-}
-
-appendImg(findUrl);
 
 // Bonus #2: Showing a map
 // Using the latitude and longitude functions and the information from this stack overflow answer, add a map element to the page.
@@ -93,7 +112,7 @@ function mapElement(obj) {
    return aDiv.prepend(iframe);
 }
 
-mapElement(atlWeather);
+
 
 // Bonus #3: Converting the sunrise/sunset
 // The sunrise and sunset information is in an odd format. In order to show it in a human readable format, you'll need to do some detective work.
@@ -131,17 +150,4 @@ function convert(obj){
    var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
 
    return convdataTime;
-   
-   // document.getElementById('datetime').innerHTML = convdataTime;
 }
-
-const sunriseSunset = [
-   `Sunrise: ${convert(atlWeather.sys.sunrise)}`,
-   `Sunset: ${convert(atlWeather.sys.sunset)}`,
-]
-
-sunriseSunset.forEach(function (sun) {
-   const aPara = document.createElement('p');
-   aPara.textContent = sun;
-   aDiv.append(aPara);
-});
